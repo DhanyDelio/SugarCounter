fdc14d5e82be75ed6f23f2bcf3cfc3f78128c80b
DhanyDelio <dhanydelio89@gmail.com>
2026-01-03 21:26:08 +0700

    setup GoogleSignIn in _layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,14 @@
 import { Tabs } from "expo-router";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { useEffect } from "react";
 
 export default function Layout(){
+    useEffect(()=> {
+        GoogleSignin.configure({
+            webClientId: "81278396720-ssvfuthc0n0dtfithcthb2u6mnusfvpe.apps.googleusercontent.com",
+            offlineAccess : true
+        })
+    },[])
     return(
         <Tabs>
             <Tabs.Screen name="login_register" 
